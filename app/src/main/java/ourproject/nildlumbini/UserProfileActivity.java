e72882cbffcb5bf2e6798a97b63b0c275c0e8cba
Ramesh pokhrel <kanxoramesh@gmail.com>
2017-09-20 17:04:26 +0545

    commited by Ramesh

---
+++ app/src/main/java/ourproject/nildlumbini/UserProfileActivity.java
@@ -0,0 +1,37 @@
+package ourproject.nildlumbini;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    Button userProfileAddButton;
+    RecyclerView userProfileRecycler;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+        userProfileAddButton= (Button) findViewById(R.id.user_profile_add_button);
+        userProfileRecycler= (RecyclerView) findViewById(R.id.user_profile_recycler);
+
+     RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(UserProfileActivity.this,LinearLayoutManager.VERTICAL,false);
+        userProfileRecycler.setLayoutManager(layoutManager);
+        userProfileRecycler.setItemAnimator(new DefaultItemAnimator());
+
+
+
+
+
+    }
+}

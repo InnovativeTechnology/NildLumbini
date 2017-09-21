9eaa1d792d5f58f96b9be5cefff34a568190bf88
Ramesh pokhrel <kanxoramesh@gmail.com>
2017-09-21 14:13:15 +0545

    add buttom commited

--- app/src/main/java/ourproject/nildlumbini/DiaLog_Add.java
+++ app/src/main/java/ourproject/nildlumbini/DiaLog_Add.java
@@ -3,32 +3,42 @@ package ourproject.nildlumbini;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DiaLog_Add extends AppCompatActivity {
-EditText bookName,fileName;
+    EditText title,article;
     Button add;
+    Spinner option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dia_log__add);
+        setContentView(R.layout.dialog_layout);
+        option= (Spinner) findViewById(R.id.option);
+        String[] opt = this.getResources().getStringArray(R.array.option);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (DiaLog_Add.this, android.R.layout.simple_dropdown_item_1line,opt);
+        option.setAdapter(adapter);
 
 
-
-        bookName= (EditText) findViewById(R.id.BookName);
-        fileName= (EditText) findViewById(R.id.pdf);
-        add= (Button) findViewById(R.id.Add);
+try {
+    title = (EditText) findViewById(R.id.title1);
+}catch (Exception e)
+{
+    e.printStackTrace();
+}
+        article= (EditText) findViewById(R.id.article);
+        add= (Button) findViewById(R.id.addButton);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Bname=bookName.getText().toString();
-                 String pdf=fileName.getText().toString();
-
 
               //  Intent intent = new Intent()
 

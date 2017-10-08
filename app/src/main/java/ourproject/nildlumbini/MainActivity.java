d53b99c436eee0330699b5bda36ac8c0ebcec669
madhu Gautam <gmadhu27@gmail.com>
2017-10-08 15:16:08 +0545

    full img

--- app/src/main/java/ourproject/nildlumbini/MainActivity.java
+++ app/src/main/java/ourproject/nildlumbini/MainActivity.java
@@ -140,8 +140,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     private void bindData() {
-
-
         bindMenu();
         database.child("UserFile").addValueEventListener(new ValueEventListener() {
             @Override

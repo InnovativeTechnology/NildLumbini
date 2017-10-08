2406cb7be1db878dfe6bbe657254f8d0c916127a
suman neupane <sumanmyon.sam@gmail.com>
2017-10-08 15:21:41 +0545

    uptodate

--- app/src/main/java/ourproject/nildlumbini/Fragment/FragmentTwo.java
+++ app/src/main/java/ourproject/nildlumbini/Fragment/FragmentTwo.java
@@ -2,20 +2,15 @@ package ourproject.nildlumbini.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-<<<<<<< HEAD
-import android.support.v7.widget.DefaultItemAnimator;
+\import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-=======
-import android.support.v7.widget.Toolbar;
->>>>>>> 605e9ddac37bb53a86a0c10206016d44a69940f6
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-<<<<<<< HEAD
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,9 +21,7 @@ import java.util.ArrayList;
 
 import ourproject.nildlumbini.Item_Adap;
 import ourproject.nildlumbini.MainActivity;
-=======
 import ourproject.nildlumbini.MyList;
->>>>>>> 605e9ddac37bb53a86a0c10206016d44a69940f6
 import ourproject.nildlumbini.R;
 import ourproject.nildlumbini.RetrieveData;
 
@@ -55,9 +48,8 @@ public class FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-<<<<<<< HEAD
-        View v = inflater.inflate(R.layout.fragment_two, container, false);
-        final RecyclerView myRecyle = (RecyclerView)v.findViewById(R.id.recycleView_fragment2);
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        final RecyclerView myRecyle = (RecyclerView)view.findViewById(R.id.recycleView_fragment2);
         RecyclerView.LayoutManager layoutManager=new GridLayoutManager(getActivity(),1);
         myRecyle.setLayoutManager(layoutManager);
         myRecyle.setItemAnimator(new DefaultItemAnimator());
@@ -101,12 +93,8 @@ public class FragmentTwo extends Fragment {
 //        });
 
         new GetDataForFragments(getActivity(), database, option, myRecyle).loadData();
-        return v;
-=======
-        View view= inflater.inflate(R.layout.fragment_two, container, false);
-        Toast.makeText(getActivity(),MyList.arrayList()+"",Toast.LENGTH_LONG).show();
         return  view;
->>>>>>> 605e9ddac37bb53a86a0c10206016d44a69940f6
+
     }
 
 }
